42c7d8210754f3e8952663426fe8961ce83e68c1
4wardAerial <4lucas.h.bertanha8@gmail.com>
2026-04-24 17:09:19 -0300

    .

--- MoreObj/script.js
+++ MoreObj/script.js
@@ -225,3 +225,22 @@ function descPessoa(a, b, c) {
 }
 descPessoa.call(4, 5, '');
 descPessoa.call(Lucas, 4, 5, '');
+
+const lista = document.querySelectorAll('li');
+
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const sections = document.querySelectorAll('section');
+const lobo = sections[sections.length - 1].innerText;
+
+const textoArray;
+
+
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
