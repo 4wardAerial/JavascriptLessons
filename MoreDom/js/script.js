019967eb07e2103cced4e9e8d7c9b58768054346
4wardAerial <4lucas.h.bertanha8@gmail.com>
2026-05-09 16:09:52 -0300

    bunch of modules

--- MoreDom/js/script.js
+++ MoreDom/js/script.js
@@ -4,8 +4,14 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimaScroll from "./modules/animacao-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/drowpdown-menu.js";
 
 initScrollSuave();
 initAnimaScroll();
 initAccordion();
 initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
